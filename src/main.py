5d3585307b6b1df3f40694fe09de486436f220a0
bytewife <ivy.ember.raine@gmail.com>
2024-04-16 16:24:45 -0700

    Add an ahead-of-time dir checker

--- src/main.py
+++ src/main.py
@@ -5,10 +5,30 @@ import variant_scoring
 import variant_shap
 import variant_summary_across_folds
 import variant_annotation
+import os
 
 
 def cli():
 	args = argmanager.fetch_main_parser()
+
+	# Check output dirs ahead of time.
+	if not args.no_scoring:
+		if not os.path.isdir(args.scoring_output_dir):
+			print(f"Error: The --scoring-output-dir directory '{args.scoring_output_dir}' does not exist.")
+			exit(1)
+	if not args.no_summary:
+		if not os.path.isdir(args.summary_output_dir):
+			print(f"Error: The --summary-output-dir directory '{args.summary_output_dir}' does not exist.")
+			exit(1)
+	if not args.no_annotation:
+		if not os.path.isdir(args.annotation_output_dir):
+			print(f"Error: The --annotation-output-dir directory '{args.annotation_output_dir}' does not exist.")
+			exit(1)
+	if not args.no_shap:
+		if not os.path.isdir(args.shap_output_dir):
+			print(f"Error: The --shap-output-dir directory '{args.shap_output_dir}' does not exist.")
+			exit(1)
+
 	if not args.no_scoring:
 		variant_scoring.main(args)
 	if not args.no_summary:
